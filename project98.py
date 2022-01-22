1e1d30125fe51d1959840340dc3db0115a241f67
AryamanGlitch <61740230+AryamanGlitch@users.noreply.github.com>
2022-01-22 12:56:24 +0530

    Add files via upload

---
+++ project98.py
@@ -0,0 +1,17 @@
+def swappingFile():
+    file1= input("Enter your 1st file path:- ")
+    file2= input("Enter your 2nd file path:- ")
+
+    with open(file1,'r') as a:
+        data_a= a.read()
+    
+    with open(file2,'r') as b:
+        data_b= b.read()
+
+    with open(file1,'w') as a:
+        a.write(data_b)
+    
+    with open(file2,'w') as b:
+        b.write(data_a)
+
+swappingFile()
